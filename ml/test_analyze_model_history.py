e593a3d32a021f841e6a0ec1a8c27d65f69b9356
Serhiy Bilonozhko <Serhii_Bilongozhko@comcast.com>
2023-07-05 23:30:18 -0400

    upd

--- ml/test_analyze_model_history.py
+++ ml/test_analyze_model_history.py
@@ -35,6 +35,35 @@ class MyTestCase(unittest.TestCase):
         fig.update_layout(height=800 * len(histories), width=1500, title_text="Side By Side Subplots")
         fig.show()
 
+    def test_compare_models_by_type(self):
+        histories = {}
+        for model_path in (find_root_dir() / 'model').iterdir():
+            co_path = model_path / 'custom_objects.json'
+            if co_path.exists():
+                with open(co_path, 'r') as f:
+                    co: Dict = json.load(f)
+                    hist = co.get('history')
+                    if hist:
+                        histories[model_path.name] = hist
+        color_mapping = defaultdict(
+            lambda: f"rgb({random.randint(0, 255)},{random.randint(0, 255)},{random.randint(0, 255)})")
+        groups = ['play', 'discard', 'clue', 'action']
+        stats = ['acc', 'val_acc']
+        fig = make_subplots(rows=len(histories), cols=2, subplot_titles=list(f"{g}_{s}" for g in groups for s in stats),
+                            vertical_spacing=0.006)
+        for i, group in enumerate(groups):
+            for (model, hist) in histories.items():
+                if group in model:
+                    for col, stat in enumerate(stats):
+                        if stat in hist:
+                            fig.add_trace(
+                                go.Scatter(y=hist[stat], name=model + "_" + stat, marker_color=color_mapping[model],
+                                           hoverlabel=dict(namelength=-1)),
+                                row=i + 1, col=col + 1
+                            )
+        fig.update_layout(height=800 * len(histories), width=1500, title_text="Side By Side Subplots")
+        fig.show()
+
 
 if __name__ == '__main__':
     unittest.main()
